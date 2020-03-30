c112a53ad6b6cfedf8d2b30e31d0b254b2ae91e7
TaeHyeongKim <uuzaza@naver.com>
2020-03-30 17:15:01 +0900

    init

---
+++ RandomCatPicture/RandomCatPicture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RandomCatPicture
+//
+//  Created by 김태형 on 2020/03/30.
+//  Copyright © 2020 김태형. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
